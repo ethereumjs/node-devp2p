444568239d55ddb753d00a375cffd8175d230f92
Latrasis <jacobapayne@gmail.com>
2015-02-14 16:39:22 -0800

    Refactor Test Format

--- test/rating.js
+++ test/rating.js
@@ -1,32 +1,29 @@
 var Network = require('../index.js'),
-	Rate = require('../lib/rating.js'),
+  Rate = require('../lib/rating.js'),
   RLP = require('rlp'),
   net = require('net'),
   assert = require('assert');
 
-module.exports = function() {
-	
-	describe('[Rating]: Rating Behavior', function () {
 
-		var peer = {};
+describe('[Rating]: Rating Behavior', function() {
+  var peer = {};
 
-    it('should modify peer to default', function (done) {
-      Rate(peer)
-      assert.equal(peer.rep, 3);
-      done();
-    });
-
-    it('should downvote peer', function(done) {
-    	Rate(peer).downvote();
-    	assert.equal(peer.rep, 2);
-    	done();
-    });
-
-    it('should upvote peer', function(done) {
-    	Rate(peer).upvote();
-    	assert.equal(peer.rep, 3);
-    	done();
-    });
-
+  it('should modify peer to default', function(done) {
+    Rate(peer)
+    assert.equal(peer.rep, 3);
+    done();
   });
-};
+
+  it('should downvote peer', function(done) {
+    Rate(peer).downvote();
+    assert.equal(peer.rep, 2);
+    done();
+  });
+
+  it('should upvote peer', function(done) {
+    Rate(peer).upvote();
+    assert.equal(peer.rep, 3);
+    done();
+  });
+
+});
