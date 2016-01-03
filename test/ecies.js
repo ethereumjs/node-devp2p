06663ecd1be747f175d0e64e89422ce515bcf38f
wanderer <mjbecze@gmail.com>
2016-01-02 21:22:23 -0500

    updateed seckp

--- test/ecies.js
+++ test/ecies.js
@@ -10,8 +10,8 @@ describe('[Network]: ECIES', function () {
   it('should create a new ECIES instance', function (done) {
     var privateKey = crypto.randomBytes(32)
     var privateKey2 = crypto.randomBytes(32)
-    var pubKey = ecdsa.createPublicKey(privateKey)
-    var pubKey2 = ecdsa.createPublicKey(privateKey2)
+    var pubKey = ecdsa.publicKeyConvert(ecdsa.publicKeyCreate(privateKey), false)
+    var pubKey2 = ecdsa.publicKeyConvert(ecdsa.publicKeyCreate(privateKey2), false)
     a = new ECIES(privateKey, pubKey, pubKey2)
     b = new ECIES(privateKey2, pubKey2, pubKey)
     done()
